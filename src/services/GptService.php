93ad5238daa7ed94aa4422972c71e3ff958ea13b
Sören Meier <soeren@s-me.ch>
2024-06-11 23:12:58 +0200

    chore: update gpt models - removed: gpt-3.5-turbo-1106 (get's automatically replaced with gpt-3.5-turbo) - removed: gpt-4-1106-preview (get's automatically replaced with gpt-4-turbo-preview - added: gpt-4-turbo - added: gpt-4o and made it the default

--- src/services/GptService.php
+++ src/services/GptService.php
@@ -40,8 +40,21 @@ class GptService extends Component
 		];
 
 		$model = $plugin->settings->gptModel;
-		if ($model === 'default')
-			$model = 'gpt-4-1106-preview';
+
+		// model overrides
+		switch ($model) {
+			case 'default':
+				$model = 'gpt-4o';
+				break;
+
+			case 'gpt-4-1106-preview':
+				$model = 'gpt-4-turbo-preview';
+				break;
+
+			case 'gpt-3.5-turbo-1106':
+				$model = 'gpt-3.5-turbo';
+				break;
+		}
 
 		try {
 			$response = $this->httpClient->post(
